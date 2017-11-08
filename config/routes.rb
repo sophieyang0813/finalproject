6811f5dd7f38182406a8541862e66bf7ed8d01fd
shinomatsu <shi968k5@gmail.com>
2017-11-08 11:52:49 +0800

    Add foods routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
   end
+   
+   resources :foods
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
