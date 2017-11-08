4a3c824282a630bdbfc2dbb9c2aa196c76ee9c64
YOUR NAME <YOUR@EMAIL.com>
2017-11-08 23:26:36 +0800

    changed postBD datetime

--- db/migrate/20171108151409_create_posts.rb
+++ db/migrate/20171108151409_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
 	def change
 		create_table :posts do |t|
 			t.string :description
-			t.string :deadline_for_collection
+			t.datetime :deadline_for_collection
 			t.integer :supporter_id
 			t.integer :charity_id  
 			t.timestamps
