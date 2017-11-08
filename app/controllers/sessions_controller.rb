20b9bfa8ad1a2edd6a5b056ba2b15f9c28e47ae6
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-11-08 18:20:27 +0800

    rewrote create method under session controller with @user, as opposed to separate @charity and @supporter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,39 @@ class SessionsController < ApplicationController
 
 
   def create 
-    @supporter = Supporter.find_by(email: params[:session][:email])
-      if @supporter && @supporter.authenticate(params[:session][:password])
-        redirect_to supporters_path
-      else 
+    @user = Supporter.find_by(email: params[:session][:email]) || Charity.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+        if @user.class == Supporter
+          redirect_to supporters_path
+        elsif @user.class == Charity
+           redirect_to charities_path
+        end
+      else
         redirect_to new_session_path
       end         
+
+
+
+
+      # if @supporter && @supporter.authenticate(params[:session][:password])
+      #   redirect_to supporters_path
+      # elsif 
+      #   redirect_to new_session_path
+      # elsif 
+      #   @charity && @charity.authenticate(params[:session][:password])
+      #   redirect_to charities_path
+      # elsif 
+      #   redirect_to new_session_path 
+      # end         
+
+      # if @charity && @charity.authenticate(params[:session][:password])
+      #   redirect_to charities_path
+      # else
+      #   redirect_to new_session_path 
+      # end 
+
+
   end 
 
 
