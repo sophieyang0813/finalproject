141713e508b263202d632a41151cb3faea107a1d
YOUR NAME <YOUR@EMAIL.com>
2017-11-08 23:10:12 +0800

    added town into charities and supporters DB

--- db/migrate/20171107104852_create_charities.rb
+++ db/migrate/20171107104852_create_charities.rb
@@ -5,7 +5,8 @@ class CreateCharities < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :email
       t.string :charity_type 
-      t.string :charity_name 
+      t.string :charity_name
+      t.string :town 
       t.string :address
       t.string :phone_num
       t.string :password_digest
