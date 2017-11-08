5f8fea6b1d2acb63aa2e95d3076f9a49ed404b1b
shinomatsu <shi968k5@gmail.com>
2017-11-08 15:30:18 +0800

    Change columnname to food_type from type in food tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107104936) do
+ActiveRecord::Schema.define(version: 20171108072655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20171107104936) do
     t.string "name"
     t.string "quantity"
     t.string "description"
-    t.string "type"
+    t.string "food_type"
     t.date "mfg_date"
     t.date "expiry_date"
     t.datetime "created_at", null: false
