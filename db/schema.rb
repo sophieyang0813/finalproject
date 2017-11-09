880d07094cba39b5a17c28839173ac68c6fb4ad3
shinomatsu <shi968k5@gmail.com>
2017-11-09 11:13:42 +0800

    Add photo in posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109030531) do
+ActiveRecord::Schema.define(version: 20171109030939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171109030531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.json "photos"
   end
 
   create_table "supporters", force: :cascade do |t|
