a1be8a786ba9e06cb0f4bd8adb50e6323ebba350
shinomatsu <shi968k5@gmail.com>
2017-11-10 20:31:40 +0800

    Add the default image

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
- 
-ActiveRecord::Schema.define(version: 20171110081855) do
 
+ActiveRecord::Schema.define(version: 20171110081855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +46,9 @@ ActiveRecord::Schema.define(version: 20171110081855) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.json "photos"
-    t.datetime "pickup_start" 
-    t.datetime "pickup_end"
     t.boolean "ordered_post", default: false, null: false
-
+    t.datetime "pickup_start"
+    t.datetime "pickup_end"
   end
 
   create_table "supporters", force: :cascade do |t|
