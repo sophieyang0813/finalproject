5f3a8b1158128a5d982491ae2dc89a9f6e38db31
shinomatsu <shi968k5@gmail.com>
2017-11-11 17:35:33 +0800

    Show charity's my page

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,9 +1,12 @@
 class CharitiesController < ApplicationController
 
+   before_action :set_charity,only: [:show,:edit,:update]
+
 
   def new
    @charity = Charity.new 
-  end 
+  end
+  
 
   def create 
     @charity = Charity.new(charity_params)
@@ -17,10 +20,31 @@ class CharitiesController < ApplicationController
 
   end 
 
+  def show
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @charity.update(charity_params)
+      redirect_to @charity
+    else
+      p @charity.errors
+      render :edit
+    end
+  end
+
+
   private 
   
   def charity_params
     params.require(:charity).permit(:last_name, :first_name, :email, :charity_type, :charity_name, :address, :phone_num, :town, :password)
   end 
+
+  def set_charity
+    @charity = Charity.find(params[:id])
+  end
 
 end 
