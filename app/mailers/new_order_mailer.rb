852399e6a1049b192d4af0d13f1020e00fc0e2b4
shinomatsu <shi968k5@gmail.com>
2017-11-13 17:34:20 +0800

    Fixed posts/index.html.erb

--- app/mailers/new_order_mailer.rb
+++ app/mailers/new_order_mailer.rb
@@ -6,7 +6,8 @@ class NewOrderMailer < ApplicationMailer
 		@order = Order.find(order_id) 
 		@post = Post.find(post_id)
 	    #once charity reserved a post, it will send email to the post charity.
-	    mail(to: @order.charity.email, subject: "We will reserved your food!" )
+	    mail(to: 'foodloveshareproject@gmail.com', subject: "We will reserved your food!" )
+	    # mail(to: @order.charity.email, subject: "We will reserved your food!" )
 
 	  end
 
@@ -15,7 +16,8 @@ class NewOrderMailer < ApplicationMailer
 	  	@charity = Charity.find(charity_id)
 	  	@order = Order.find(order_id)
 	    #once charity reserved a post, it will send email to the post supporter.
-	    mail(to: @order.post.supporter.email, subject: "Someone is interested in your post!")
+	    mail(to: 'foodloveshareproject@gmail.com', subject: "Someone is interested in your post!")
+        # mail(to: @order.post.supporter.email, subject: "Someone is interested in your post!")
 
 	  end
 	end
