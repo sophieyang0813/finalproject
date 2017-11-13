c0b81841a429f0f4ceaf014a3722e2c872d40764
Hui Chuin <lhuichuin@gmail.com>
2017-11-13 18:24:13 +0800

    added lat and lng to charities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113064341) do
+ActiveRecord::Schema.define(version: 20171113102233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20171113064341) do
     t.datetime "updated_at", null: false
     t.json "photos"
     t.string "state"
+    t.float "longitude"
+    t.float "latitude"
   end
 
   create_table "orders", force: :cascade do |t|
