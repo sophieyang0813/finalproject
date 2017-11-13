b53c7aae9495175c83384e7d98cfc97c9aebd8bd
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-11-13 13:22:03 +0800

    fix conflict in index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111093811) do
+ActiveRecord::Schema.define(version: 20171112110809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20171111093811) do
     t.datetime "updated_at", null: false
     t.json "photos"
     t.string "state"
-
   end
 
   create_table "orders", force: :cascade do |t|
@@ -70,7 +69,6 @@ ActiveRecord::Schema.define(version: 20171111093811) do
     t.float "longitude"
     t.json "photos"
     t.string "state"
-
   end
 
 end
